7101fe23a1074e314fc4afbd8c76ee78216f6473
dmitriy10291029 <dmitriy.lapshin10@gmail.com>
2024-03-20 23:17:05 +0300

    Add new Solution338

--- src/main/java/leetcode/Solution338.java
+++ src/main/java/leetcode/Solution338.java
@@ -7,23 +7,23 @@ import java.util.Arrays;
 public class Solution338 {
     public int[] countBits(int n) {
         int[] answer = new int[n + 1];
-        int maxDegree = 16;
-        for (int i = 0; i <= n; ++i) {
-            int factor = 1;
-            int sumOfOnes = 0;
-            for (int j = 1; j <= maxDegree; ++j) {
-                if ((i & factor) > 0) {
-                    sumOfOnes++;
-                }
-                factor <<= 1;
+        answer[0] = 0;
+        int j = 1;
+        int k = 0;
+
+        for (int i = 1; i < n + 1; i++) {
+            if (i == j) {
+                k = 0;
+                j *= 2;
             }
-            answer[i] = sumOfOnes;
+            answer[i] = answer[k] + 1;
+            k++;
         }
         return answer;
     }
 
     public static void main(String[] args) {
         Solution338 s = new Solution338();
-        System.out.println(Arrays.toString(s.countBits(15)));
+        System.out.println(Arrays.toString(s.countBits(10)));
     }
 }
