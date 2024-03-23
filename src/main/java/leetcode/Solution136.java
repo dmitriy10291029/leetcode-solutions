4c6ae65dcff8b5cc371bee25a97be935c566f8ec
dmitriy10291029 <dmitriy.lapshin10@gmail.com>
2024-03-23 16:24:30 +0300

    Add new Solution

---
+++ src/main/java/leetcode/Solution136.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Solution136 {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+}
