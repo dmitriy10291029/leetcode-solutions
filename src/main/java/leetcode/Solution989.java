d3281fc78a67efd11d3ba35bddc9c5c18f1b2d35
dmitriy10291029 <dmitriy.lapshin10@gmail.com>
2024-03-23 20:32:56 +0300

    Add new Solution

---
+++ src/main/java/leetcode/Solution989.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution989 {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> answer = new LinkedList<>();
+        int extra = 0;
+        for (int i = num.length - 1; i >= 0 || k > 0; i--) {
+            int sum = extra + (k % 10);
+            if (i >= 0) {
+                sum += num[i];
+            }
+            k /= 10;
+            extra = sum / 10;
+            answer.add(0, sum % 10);
+        }
+        if (extra != 0) {
+            answer.add(0, extra);
+        }
+
+        return answer;
+    }
+}
