a610a4761d8db4efc8643b5d2496911263166370
dmitriy10291029 <dmitriy.lapshin10@gmail.com>
2024-03-23 13:50:30 +0300

    Add new Solution

---
+++ src/main/java/leetcode/Solution35.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+// https://leetcode.com/problems/search-insert-position/
+
+public class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        int mid = right / 2;
+
+        while (right - left > 1) {
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+            mid = (right + left) / 2;
+        }
+
+        return target > nums[mid] ? mid + 1 : mid;
+    }
+}
---
+++ src/test/java/leetcode/Solution35Test.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution35Test {
+    Solution35 s = new Solution35();
+
+    @Test
+    void Test1() {
+        int[] nums = {1,3,5,6};
+        assertEquals(2, s.searchInsert(nums, 5));
+    }
+
+    @Test
+    void Test2() {
+        int[] nums = {1,3,5,6};
+        assertEquals(1, s.searchInsert(nums, 2));
+    }
+
+    @Test
+    void Test3() {
+        int[] nums = {1,3,5,6};
+        assertEquals(4, s.searchInsert(nums, 7));
+    }
+
+    @Test
+    void Test4() {
+        int[] nums = {1,3,5,6,8,9,15,16,20,26,40};
+        assertEquals(8, s.searchInsert(nums, 20));
+    }
+
+    @Test
+    void Test5() {
+        int[] nums = {1,3,5,6,8,9,15,16,20,26,40};
+        assertEquals(8, s.searchInsert(nums, 18));
+    }
+
+    @Test
+    void Test6() {
+        int[] nums = {1,3,5,6};
+        assertEquals(0, s.searchInsert(nums, 0));
+    }
+}
