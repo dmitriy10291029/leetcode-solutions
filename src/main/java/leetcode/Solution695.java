1cd471fba219be0eb27fefe5bde5f6dbb6315f67
dmitriy10291029 <dmitriy.lapshin10@gmail.com>
2024-07-23 14:53:20 +0300

    Add new solution

---
+++ src/main/java/leetcode/Solution695.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class Solution695 {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                if (grid[y][x] == 1) {
+                    maxArea = Math.max(maxArea, dfs(x, y, grid, 0));
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
+    private int dfs(int x, int y, int[][] grid, int area) {
+        if (0 <= x && x < grid[0].length && 0 <= y && y < grid.length && grid[y][x] == 1) {
+            grid[y][x] = 0;
+            area++;
+            area = dfs(x, y + 1, grid, area);
+            area = dfs(x, y - 1, grid, area);
+            area = dfs(x + 1, y, grid, area);
+            area = dfs(x - 1, y, grid, area);
+        }
+        return area;
+    }
+}
